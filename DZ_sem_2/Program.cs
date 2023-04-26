704e35d0cf0a51a64cbde3b5d066e5d026b5b1bd
Djem79 <tdm.979@gmail.com>
2023-04-26 20:29:56 +0300

    Решение задачи №10

---
+++ DZ_sem_2/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+/*void SecDig()
+{
+    Console.WriteLine("Input your 3 digit number ");
+    int user_number = Convert.ToInt32(Console.ReadLine());
+
+    if (user_number > 100 && user_number < 1000)
+    {
+        int second_digit = (user_number / 10) % 10;
+        Console.WriteLine("Second digit is: " + second_digit);
+    }
+    else
+    {
+        Console.WriteLine("You enter wrong number.");
+    }
+}
+SecDig();*/
+
