ace7e990e688ff96cc1a095df6846bb3b4f79e22
Djem79 <tdm.979@gmail.com>
2023-04-26 22:28:23 +0300

    промежуточное решение задачи

--- DZ_sem_2/Program.cs
+++ DZ_sem_2/Program.cs
@@ -21,3 +21,50 @@
 }
 SecDig();*/
 
+/*Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6*/
+
+
+void thirdDigit()
+{
+    Console.WriteLine("Input your number:");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num > 99)
+    {   
+        Console.WriteLine("Third digit is: " + num % 10);
+    }
+    else
+    {
+        Console.WriteLine ("Your number have no third digit");
+    }
+}
+thirdDigit();
+
+/*using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int thirdDigit = (int)(number / Math.Pow(10, 2)) % 10;
+
+        if (thirdDigit == 0)
+        {
+            Console.WriteLine("В вашем числе нет третьей цифры.");
+        }
+        else
+        {
+            Console.WriteLine("Третья цифра вашего числа: " + thirdDigit);
+        }
+
+        Console.ReadKey();
+    }
+}\
+*/
+
