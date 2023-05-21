d903321eff83041c5d3951c64e787ed4645fe529
Djem79 <tdm.979@gmail.com>
2023-05-21 23:27:04 +0300

    Решение задачи №25

---
+++ DZ_sem_4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+
+void StepenNumber (string[] args)
+{
+    Console.WriteLine("Enter number A: ");
+    double a = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Enter number B: ");
+    int b = Convert.ToInt32(Console.ReadLine());
+
+    double result = 1;
+
+    for (int i = 1; i <= b; i++)
+    {
+        result *= a;
+    } 
+    Console.WriteLine("Your number is: " + result);
+}
+StepenNumber(args);
