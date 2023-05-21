2ebcdb97937c416d134f3ed0cfb414e652d94fd1
Djem79 <tdm.979@gmail.com>
2023-05-22 00:14:26 +0300

    Решение задачи №27

--- DZ_sem_4/Program.cs
+++ DZ_sem_4/Program.cs
@@ -1,6 +1,6 @@
-﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
-
+/*
 void StepenNumber (string[] args)
 {
     Console.WriteLine("Enter number A: ");
@@ -18,3 +18,28 @@ void StepenNumber (string[] args)
     Console.WriteLine("Your number is: " + result);
 }
 StepenNumber(args);
+*/
+
+//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+/*
+void FindSum (string[] args)
+{
+    Console.WriteLine("Input your number: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int sum = 0;
+    int temp;
+
+    while(number > 0)
+    {
+        temp = number % 10;
+        sum += temp;
+        number /= 10;
+    }
+    Console.WriteLine("Sum of your number is: " + sum);
+}
+FindSum(args);
+*/
+
