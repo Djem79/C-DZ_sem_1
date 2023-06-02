23d1ad82a6ace2e0ef0d8e1581e8a889025624a0
Djem79 <tdm.979@gmail.com>
2023-06-02 18:15:50 +0300

    Решение задачи 50

--- DZ_sem_7/Program.cs
+++ DZ_sem_7/Program.cs
@@ -5,7 +5,7 @@ m = 3, n = 4.
 8 7,8 -7,1 9
 */
 
-
+/*
 double [,] Create2Array()
 {
     Console.WriteLine("Input count of rows: ");
@@ -44,3 +44,50 @@ void Print2Array(double [,] array2d)
 
 double[,] new2dArray = Create2Array();
 Print2Array(new2dArray);
+*/
+
+
+/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+*/
+
+/*
+double[,] array = {
+    {1, 4, 7, 2},
+    {5, 9, 2, 3},
+    {8, 4, 2, 4}
+};
+
+Console.Write("Input number of row: ");
+int row = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number of column: ");
+int col = Convert.ToInt32(Console.ReadLine());
+
+double value = double.MinValue;
+int numRows = array.GetLength(0);
+int numCols = array.GetLength(1);
+
+for (int i = 0; i < numRows; i++)
+{
+    for (int j = 0; j < numCols; j++)
+    {
+        if (1 == row && j == col)
+        {
+            value = array[i, j];
+            break;
+        }
+    }
+}
+if (value == double.MinValue)
+{
+    Console.WriteLine("Element in this position does not exist");
+}
+else
+{
+    Console.WriteLine("Value of element: " + value);
+}
+*/
