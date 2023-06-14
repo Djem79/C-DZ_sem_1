cad1a279ed128c42007ec32c1f03eff5c7182890
Djem79 <tdm.979@gmail.com>
2023-06-15 01:49:48 +0300

    Решение домашней задачи № 68

--- DZ_sem_9/Program.cs
+++ DZ_sem_9/Program.cs
@@ -28,6 +28,7 @@ NaturalNumbers();
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
 */
+/*
 void FindSum()
 {
     Console.Write("Input M value: ");
@@ -52,3 +53,36 @@ void FindSum()
     }
 }
 FindSum();
+*/
+
+/*
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+
+int AckermannFunction(int m, int n) 
+{
+    if (m == 0) 
+    {
+        return n + 1;
+    } else if (m > 0 && n == 0) 
+    {
+        return AckermannFunction(m - 1, 1);
+    } 
+    else 
+    {
+        return AckermannFunction(m - 1, AckermannFunction(m, n - 1));
+    }
+}
+
+
+Console.Write("Input m: ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Input n: ");
+int n = int.Parse(Console.ReadLine());
+
+int result = AckermannFunction(m, n);
+Console.WriteLine("Result for Ackerman function is m={0} и n={1}: {2}", m, n, result);
