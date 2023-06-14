2588df02648a9cf58de5a46aa3378e82912cf2e9
Djem79 <tdm.979@gmail.com>
2023-06-15 00:20:40 +0300

    Решение домашней задачи № 58

--- DZ_sem_8/Program.cs
+++ DZ_sem_8/Program.cs
@@ -54,7 +54,7 @@ for (int row = 0; row < numRows; row++)
 5 2 6 7
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 */
-
+/*
 int[,] array = {
     { 1, 4, 7, 2 },
     { 5, 9, 2, 3 },
@@ -84,3 +84,62 @@ for (int row = 0; row < numRows; row++)
 }
 
 Console.WriteLine("Minimum sum element row: " + (minSumRow + 1));
+*/
+
+/*Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+/*
+int[,] matrix1 = {
+{ 2, 4 },
+{ 3, 2 }
+};
+
+int[,] matrix2 = {
+{ 3, 4 },
+{ 3, 3 }
+};
+
+int numRows1 = matrix1.GetLength(0);
+int numCols1 = matrix1.GetLength(1);
+
+int numRows2 = matrix2.GetLength(0);
+int numCols2 = matrix2.GetLength(1);
+
+if(numCols1 != numRows2)
+{
+    Console.WriteLine ("Imposible!!");
+    return;
+}
+
+int [,] product = new int[numRows1, numCols2];
+
+for (int row = 0; row < numRows1; row++)
+{
+    for( int col = 0; col < numCols2; col++)
+    {
+        int sum = 0;
+        for( int k = 0; k < numCols1; k++)
+        {
+            sum += matrix1[row, k] * matrix2[k, col];
+        }
+        product[row, col] = sum;
+    }
+}
+
+Console.WriteLine("Sum of matrix: ");
+
+for(int row = 0; row < numRows1; row++)
+{
+    for(int col = 0; col < numCols2; col++)
+    {
+        Console.Write(product[row, col] + " ");
+    }
+    Console.WriteLine();
+}
+*/
